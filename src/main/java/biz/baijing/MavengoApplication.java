202256fa5a9d8a3290ef224d3349889ca86837f7
RR1335 <odyli@126.com>
2025-05-19 22:25:25 -0400

    start maven modelues dev

---
+++ src/main/java/biz/baijing/MavengoApplication.java
@@ -0,0 +1,13 @@
+package biz.baijing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavengoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavengoApplication.class, args);
+    }
+
+}
